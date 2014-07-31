323587cb97556fc2d99905670782596c9c65ac4d
Raymond Cuenen <Raymond.Cuenen@gmail.com>
2014-07-31 11:06:37 +0200

    Add the Gliffy button to the Wiki editing page

---
+++ init.rb
@@ -0,0 +1,9 @@
+require 'redmine'
+require_dependency 'gliffy_helper_patch'
+
+Redmine::Plugin.register :redmine_gliffy do
+  name 'Redmine Gliffy plugin'
+  author 'R. Cuenen'
+  description 'A plugin for including Gliffy Diagrams'
+  version '0.0.1'
+end
---
+++ lib/gliffy_helper_patch.rb
@@ -0,0 +1,33 @@
+require_dependency "redmine/wiki_formatting/textile/helper"
+
+module Redmine
+  module WikiFormatting
+    module Textile
+      module Helper
+
+        def heads_for_wiki_formatter_with_gliffy
+          heads_for_wiki_formatter_without_gliffy
+          unless @heads_for_wiki_formatter_gliffy_included
+            content_for :header_tags do
+              out = stylesheet_link_tag("gliffy.css", :plugin => "redmine_gliffy")
+              out += javascript_tag <<-javascript_tag
+              jsToolBar.prototype.elements.gliffy = {
+                type: 'button',
+                title: 'Gliffy Diagram',
+                fn: {
+                  wiki: function() { this.encloseSelection("{{gliffy(", ")}}") }
+                }
+              }
+              javascript_tag
+              out
+            end
+            @heads_for_wiki_formatter_gliffy_included = true
+          end
+        end
+
+        alias_method_chain :heads_for_wiki_formatter, :gliffy
+        
+      end
+    end
+  end
+end
